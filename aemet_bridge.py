f4e844c7b56f21ce2b1dbeb980978dc3810cebd1
kenkeiras <me@codigoparallevar.com>
2019-09-14 21:16:24 +0200

    Initial commit.

---
+++ aemet_bridge.py
@@ -0,0 +1,107 @@
+import os
+import logging
+import json
+import urllib.request
+
+from plaza_bridge import (
+    PlazaBridge,  # Import bridge functionality
+    CallbackBlockArgument,  # Needed for argument definition
+    VariableBlockArgument,
+    BlockContext,
+)
+
+bridge = PlazaBridge(
+    name="AEMET",
+    endpoint=os.environ['BRIDGE_ENDPOINT'],
+    is_public=True,
+)
+
+API_KEY=os.environ['API_KEY']  # Can be obtained at https://opendata.aemet.es/centrodedescargas/inicio
+
+# Get location codes
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                       "municipalities_sorted.tsv")) as f:
+    MUNICIPALITIES = []
+    for line in f.readlines()[1:]: # Ignore header
+        line = line.strip()
+        if len(line) == 0:
+            continue
+
+        code_autonomy, code_province, code_municipality, code_dc, place = line.strip().split("\t")
+        full_id = '/'.join(map(str, [code_autonomy, code_province, code_municipality, code_dc]))
+        MUNICIPALITIES.append({"id": full_id, "name": place})
+
+def to_aemet_id(full_id):
+    _code_autonomy, code_province, code_municipality, _code_dc = full_id.split('/')
+    return code_province + code_municipality
+
+@bridge.callback
+def get_locations(_extra_data):
+    logging.info('DATA locations')
+    return MUNICIPALITIES
+
+@bridge.getter(
+    id="get_today_max_in_place",
+    message="Get today's max temperature for %1",
+    arguments=[
+        CallbackBlockArgument(str, get_locations),
+    ],
+    block_result_type=str,
+)
+def get_max_prediction(place_code, extra_data):
+    # Getter logic
+    logging.info('GET max-prediction on {}'.format(place_code))
+    return get_all_prediction(place_code, extra_data)['prediccion']['dia'][0]['temperatura']['maxima']
+
+@bridge.getter(
+    id="get_today_min_in_place",
+    message="Get today's min temperature for %1",
+    arguments=[
+        CallbackBlockArgument(str, get_locations),
+    ],
+    block_result_type=str,
+)
+def get_min_prediction(place_code, extra_data):
+    logging.info('GET min-prediction on {}'.format(place_code))
+    return get_all_prediction(place_code, extra_data)['prediccion']['dia'][0]['temperatura']['minima']
+
+
+@bridge.operation(
+    id="get_today_rain_probability_in_place",
+    message="Get today's rain probabilities for %1. Save to %2",
+    arguments=[
+        CallbackBlockArgument(str, get_locations),
+        VariableBlockArgument(list),
+    ],
+    save_to=BlockContext.ARGUMENTS[1],
+)
+def get_rain_prediction(place_code, extra_data):
+    logging.info('GET rain-prediction on {}'.format(place_code))
+    return get_all_prediction(place_code, extra_data)['prediccion']['dia'][0]['probPrecipitacion']
+
+# @bridge.operation(
+#     id="get_all_data_from_place",
+#     message="Get all data for %1. Save to %2",
+#     arguments=[
+#         CallbackBlockArgument(str, get_locations),
+#         VariableBlockArgument(list),
+#     ],
+#     save_to=BlockContext.ARGUMENTS[1],
+# )
+def get_all_prediction(place_code, extra_data):
+    # Getter logic
+    r = urllib.request.urlopen("https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/{id}?api_key={api_key}"
+                               .format(id=to_aemet_id(place_code), api_key=API_KEY))
+    pointers = json.loads(r.read())
+    data_req = urllib.request.urlopen(pointers['datos'])
+
+    data = data_req.read().decode('latin1')
+    return json.loads(data)[0]
+
+
+if __name__ == '__main__':
+   logging.basicConfig(format="%(asctime)s - %(levelname)s [%(filename)s] %(message)s")
+   logging.getLogger().setLevel(logging.INFO)
+
+   logging.info('Starting bridge')
+   bridge.run()
